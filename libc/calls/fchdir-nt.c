514600e2af03d6d468e0b3b788c1510b35f4f5b4
starccy <452276725@qq.com>
2026-09-06 20:59:58 +0800

    Fix fchdir() on Windows storing a \\?\ prefixed cwd

--- libc/calls/fchdir-nt.c
+++ libc/calls/fchdir-nt.c
@@ -21,6 +21,7 @@
 #include "libc/calls/syscall_support-nt.internal.h"
 #include "libc/dce.h"
 #include "libc/nt/files.h"
+#include "libc/str/str.h"
 #include "libc/sysv/errfuns.h"
 #include "libc/sysv/pib.h"
 
@@ -30,8 +31,31 @@ textwindows int sys_fchdir_nt(int dirfd) {
   char16_t dir[PATH_MAX];
   if (!__isfdkind(dirfd, kFdFile))
     return ebadf();
-  return sys_chdir_nt16(
-      dir, GetFinalPathNameByHandle(__get_pib()->fds.p[dirfd].handle, dir,
-                                    ARRAYLEN(dir),
-                                    kNtFileNameNormalized | kNtVolumeNameDos));
+  uint32_t len = GetFinalPathNameByHandle(
+      __get_pib()->fds.p[dirfd].handle, dir, ARRAYLEN(dir),
+      kNtFileNameNormalized | kNtVolumeNameDos);
+
+  // GetFinalPathNameByHandle() always answers in the \\?\ namespace,
+  // which disables win32 path normalization, so SetCurrentDirectory()
+  // would store the prefix verbatim and every child spawned afterwards
+  // would inherit a \\?\C:\x cwd most programs can't parse. chdir()
+  // doesn't have this problem since __mkntpath() only keeps the prefix
+  // on paths too long for the classic limit, so do the same here
+  if (len && len < ARRAYLEN(dir) && dir[0] == '\\' && dir[1] == '\\' &&
+      dir[2] == '?' && dir[3] == '\\') {
+    if (dir[4] == 'U' && dir[5] == 'N' && dir[6] == 'C' && dir[7] == '\\') {
+      // \\?\UNC\srv\share\x -> \\srv\share\x
+      if (len - 6 < 260) {
+        memmove(dir + 2, dir + 8, (len - 8 + 1) * sizeof(char16_t));
+        len -= 6;
+      }
+    } else {
+      // \\?\C:\x -> C:\x
+      if (len - 4 < 260) {
+        memmove(dir, dir + 4, (len - 4 + 1) * sizeof(char16_t));
+        len -= 4;
+      }
+    }
+  }
+  return sys_chdir_nt16(dir, len);
 }
